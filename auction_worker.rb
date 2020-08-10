41b92d17ab80eb959bb3745f6ccb1b7fb3cfb3f7
Andrew Reid <areid.sj@gmail.com>
2020-08-10 09:49:53 -0600

    Refactor worker to hard code url for dev and testing purposes

--- auction_worker.rb
+++ auction_worker.rb
@@ -1,11 +1,16 @@
 require 'sidekiq'
 require 'sidekiq-scheduler'
+require 'faraday'
+require 'dotenv'
+Dotenv.load('var.env')
 
 
 class AuctionWorker
   include Sidekiq::Worker
 
   def perform
-    puts 'Hello World!'
+    url = 'http://127.0.0.0:5000/items/winners'
+    response = Faraday.post(url)
+    puts response.body
   end
 end
